9a6e1110f35dc58c7d141d1efe00e7daeea93b50
Cody Kremer <ckremer99@outlook.com>
2024-08-06 11:24:36 -0700

    added middleware

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express')
 const app = express() 
 const mongoose = require('mongoose')
 const cors = require('cors')
+const methodOverride = require('method-override')
 
 mongoose.connect(process.env.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(methodOverride('_method'))
 
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
